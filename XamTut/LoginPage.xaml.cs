a4d9ff6c133b561bda963e1ced56a4a6b39901b9
andrew watson <Andrew@andrews-MacBook-Pro.local>
2017-12-03 02:52:23 -0500

    Began work on Login page Layout

--- XamTut/LoginPage.xaml.cs
+++ XamTut/LoginPage.xaml.cs
@@ -7,9 +7,27 @@ namespace XamTut
 {
     public partial class LoginPage : ContentPage
     {
+        
+
         public LoginPage()
         {
             InitializeComponent();
+            //var contLay = new StackLayout();
+            //contLay.Children.Add(contLay);
+            //var relativeLayout = new RelativeLayout();
+            //var login = new Label{Text="Login: ", FontSize=36,  };
+            //relativeLayout.Children.Add(login, );
+
+
+
+            //this.Content = contLay;
         }
+
+
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
     }
 }
