1908e59dea486c01447110ddd92e152bd4d7e09b
andrew watson <Andrew@andrews-MacBook-Pro.local>
2017-12-29 23:06:48 -0500

    Started trying to figure out how to store messages and userprofiles in database. Added button to test HTTP request on toolspage

--- XamTut/ToolsPage.xaml.cs
+++ XamTut/ToolsPage.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Net.Http;
 
+using System.Collections.Generic;
 using Xamarin.Forms;
+
+
 /*  This page will hold tiles that link to tools in the application
  * 
  * 
@@ -12,8 +15,15 @@ namespace XamTut
 {
     public partial class ToolsPage : ContentPage
     {
+        
+        HttpClient client = new HttpClient();
+        static Uri local = new Uri("http://localhost:1337");
+        HttpRequestMessage requestMessage = new HttpRequestMessage { RequestUri = local };
+
+        HttpResponseMessage responseMessage = new HttpResponseMessage();
         Messages messages = new Messages();
         ConversationPage conversation = new ConversationPage("Andrew");
+
         public ToolsPage()
         {
             InitializeComponent();
@@ -24,10 +34,25 @@ namespace XamTut
          *  Temporary navigation handler to Test Messages page. Called
          *  after pressing Messages button
          */
+
+
+
+
         void ToMessagesPage(object sender, System.EventArgs e)
         {
             Navigation.PushAsync(messages);
 
         }
+
+        async void Test_Node(object sender, System.EventArgs e)
+        {
+
+            //client.SendAsync(requestMessage);
+            var response = await client.GetAsync(local);
+            await DisplayAlert((response.Content.ToString()), "ok", "ok");
+
+        }
+
     }
 }
+ 
